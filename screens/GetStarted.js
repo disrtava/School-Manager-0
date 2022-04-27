9a77c6a8587864fd11a079f07edb206042ca31a5
disrtava <82456217+disrtava@users.noreply.github.com>
2022-04-27 19:51:29 +0530

    Add files via upload

---
+++ screens/GetStarted.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TouchableOpacity, StyleSheet,Text,View} from 'react-native'
+
+export default class GetStarted extends React.Component {
+    render(){
+        return(
+            <View>
+                <TouchableOpacity>
+                    <Text>TimeTable</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
